32251bf32e5babef6b723b05427f48e76ab616a2
Mahmud Ridwan <m@hjr265.me>
2023-12-21 22:04:15 +0600

    Improve cppreference.com clean HTML filter

--- site/com.cppreference/cleanhtml.go
+++ site/com.cppreference/cleanhtml.go
@@ -1,6 +1,7 @@
 package cppreference
 
 import (
+	"strconv"
 	"strings"
 
 	"github.com/FurqanSoftware/blink/pipe"
@@ -9,8 +10,40 @@ import (
 )
 
 func CleanHTML() pipe.Filter {
+	codeLineBreakReplacer := strings.NewReplacer(
+		"<br>\n", "\n",
+		"<br>", "\n",
+		"\n</p>\n", "</p>\n",
+	)
+
 	return pipe.FilterFunc(func(_ pipe.Context, p pipe.Page) (pipe.Page, error) {
+		p.Doc.Find("div > span.source-c, div > span.source-cpp").Each(func(_ int, s *goquery.Selection) {
+			p := s.Parent()
+			p.AddClass(s.AttrOr("class", ""))
+			n := p.Nodes[0]
+			n.DataAtom = atom.Pre
+			n.Data = "pre"
+			html, err := s.Html()
+			if err != nil {
+				return
+			}
+			s.SetHtml(codeLineBreakReplacer.Replace(html))
+		})
+
 		p.Doc.Find("h1").Remove()
+
+		p.Doc.Find(".t-dcl-rev-aux td[rowspan]").Each(func(_ int, s *goquery.Selection) {
+			rowspan, err := strconv.Atoi(s.AttrOr("rowspan", ""))
+			if err != nil {
+				return
+			}
+			if rowspan > 3 {
+				n := s.ParentsFiltered("tbody").First().Find("tr").Length()
+				if n > 1 {
+					s.SetAttr("rowspan", strconv.Itoa(n))
+				}
+			}
+		})
 
 		p.Doc.Find("#top, #mw-js-message, #siteSub, #contentSub, .printfooter, .t-navbar, .editsection, #toc, .t-dsc-sep, .t-dcl-sep, #catlinks, .ambox-notice, .mw-cite-backlink, .t-sdsc-sep:first-child:last-child, .t-example-live-link, .t-dcl-rev-num > .t-dcl-rev-aux ~ tr:not(.t-dcl-rev-aux) > td:nth-child(2)").Remove()
 
@@ -33,13 +66,6 @@ func CleanHTML() pipe.Filter {
 
 		p.Doc.Find("table[style], th[style], td[style]").RemoveAttr("style")
 
-		p.Doc.Find("div > span.source-cpp").Each(func(_ int, s *goquery.Selection) {
-			s.Parent().AddClass(s.AttrOr("class", ""))
-			n := s.Parent().Nodes[0]
-			n.DataAtom = atom.Pre
-			n.Data = "pre"
-		})
-
 		p.Doc.Find("pre").Each(func(_ int, s *goquery.Selection) {
 			lang := "c"
 			if strings.Contains(s.AttrOr("class", ""), "cpp") || strings.Contains(s.Parent().AttrOr("class", ""), "cpp") {
@@ -47,7 +73,10 @@ func CleanHTML() pipe.Filter {
 			}
 			s.SetAttr("data-language", lang)
 			s.RemoveAttr("class")
-			s.SetText(strings.ReplaceAll(s.Text(), "\u00a0", " "))
+			text := s.Text()
+			text = strings.ReplaceAll(text, "\t", "        ")
+			text = strings.ReplaceAll(text, "\u00a0", " ")
+			s.SetText(text)
 		})
 
 		return p, nil
