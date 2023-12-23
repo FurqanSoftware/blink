f36767b2bfc993afa9453660e6ae8163b21137c7
Mahmud Ridwan <m@hjr265.me>
2023-12-23 10:07:08 +0600

    Improve Python marks generation

--- site/org.python.docs/marks.go
+++ site/org.python.docs/marks.go
@@ -8,7 +8,22 @@ import (
 )
 
 func Marks() pipe.Filter {
-	headingNumberRegexp := regexp.MustCompile(`\A[\d\.]+ `)
+	kindReplacements := map[string]string{
+		"contextvars — Context Variables":          "Context Variables",
+		"Cryptographic":                            "Cryptography",
+		"Custom Interpreters":                      "Interpreters",
+		"Data Compression & Archiving":             "Data Compression",
+		"email — An email & MIME handling package": "Email",
+		"Generic Operating System":                 "Operating System",
+		"Graphical User Interfaces with Tk":        "Tk",
+		"Internet Data Handling":                   "Internet Data",
+		"Internet Protocols & Support":             "Internet",
+		"Interprocess Communication & Networking":  "Networking",
+		"Program Frameworks":                       "Frameworks",
+		"Structured Markup Processing Tools":       "Structured Markup",
+	}
+
+	headingNumberRegexp := regexp.MustCompile(`\A[\d\.]+\s+`)
 	dashSuffixRegexp := regexp.MustCompile(` [` + "\u2013\u2014" + `].+\z`)
 
 	return pipe.FilterFunc(func(x pipe.Context, p pipe.Page) (pipe.Page, error) {
@@ -22,42 +37,46 @@ func Marks() pipe.Filter {
 
 		kind := ""
 		switch {
-		case strings.HasPrefix(x.URL.Path, "/reference"):
+		case strings.HasPrefix(x.Path, "/reference"):
 			kind = "Language Reference"
-		case strings.HasPrefix(x.URL.Path, "/c-api"):
+		case strings.HasPrefix(x.Path, "/c-api"):
 			kind = "Python/C API"
-		case strings.HasPrefix(x.URL.Path, "/tutorial"):
+		case strings.HasPrefix(x.Path, "/tutorial"):
 			kind = "Tutorial"
-		case strings.HasPrefix(x.URL.Path, "/distributing"):
+		case strings.HasPrefix(x.Path, "/distributing"):
 			kind = "Software Packaging & Distribution"
-		case strings.HasPrefix(x.URL.Path, "/distutils"):
+		case strings.HasPrefix(x.Path, "/distutils"):
 			kind = "Software Packaging & Distribution"
-		case strings.HasPrefix(x.URL.Path, "/glossary"):
+		case strings.HasPrefix(x.Path, "/glossary"):
 			kind = "Glossary"
-		case !strings.HasPrefix(x.URL.Path, "/library/") || strings.HasPrefix(x.URL.Path, "/library/index"):
+		case !strings.HasPrefix(x.Path, "/library/") || strings.HasPrefix(x.Path, "/library/index"):
 			kind = "Basics"
-		case strings.HasPrefix(x.URL.Path, "/library/logging"):
+		case strings.HasPrefix(x.Path, "/library/logging"):
 			kind = "Logging"
-		case strings.HasPrefix(x.URL.Path, "/library/asyncio"):
+		case strings.HasPrefix(x.Path, "/library/asyncio"):
 			kind = "Asynchronous I/O"
-		default:
-			kind = "???"
 		}
 
-		// type = at_css('.related a[accesskey="U"]').content
+		kind = p.Doc.Find(`.related a[accesskey="U"]`).Text()
 
-		// if type == 'The Python Standard Library'
-		//   type = at_css('h1').content
-		// elsif type.include?('I/O') || %w(select selectors).include?(name)
-		//   type = 'Input/ouput'
-		// elsif type.start_with? '19'
-		//   type = 'Internet Data Handling'
-		// end
+		if kind == "The Python Standard Library" {
+			kind = p.Doc.Find("h1").Text()
+		} else if strings.Contains(kind, "I/O") || name == "select" || name == "selectors" {
+			kind = "Input/output"
+		} else if strings.HasPrefix(kind, "19") {
+			kind = "Internet Data Handling"
+		}
 
-		// type.remove! %r{\A\d+\.\s+} unless include_h2? # remove list number
-		// type.remove! "\u{00b6}" # remove paragraph character
-		// type.sub! ' and ', ' & '
-		// [' Services', ' Modules', ' Specific', 'Python '].each { |str| type.remove!(str) }
+		kind = headingNumberRegexp.ReplaceAllString(kind, "")
+		kind = strings.ReplaceAll(kind, "\u00b6", "")
+		kind = strings.ReplaceAll(kind, " and ", " & ")
+		for _, k := range []string{" Services", " Modules", " Specific", "Python "} {
+			kind = strings.ReplaceAll(kind, k, "")
+		}
+
+		if v, ok := kindReplacements[kind]; ok {
+			kind = v
+		}
 
 		p.Marks = append(p.Marks, pipe.Mark{
 			Kind: kind,
