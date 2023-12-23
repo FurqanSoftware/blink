49da902f6f97c8c0eb9d506534dbc70968938483
Mahmud Ridwan <m@hjr265.me>
2023-12-23 11:20:15 +0600

    Use first match in container filter

--- pipe/container.go
+++ pipe/container.go
@@ -2,7 +2,7 @@ package pipe
 
 func Container(selector string) Filter {
 	return FilterFunc(func(x Context, p Page) (Page, error) {
-		html, _ := p.Doc.Find(selector).Html()
+		html, _ := p.Doc.Find(selector).First().Html()
 		p.Doc.Find("body").SetHtml(html)
 		return p, nil
 	})
